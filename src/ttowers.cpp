02bb622b003ad0ebb575c6df7e8b48740230a798
Sang Pham <greent.1301200@gmail.com>
2021-04-23 10:42:50 +0700

    Add solution for case -1

--- src/ttowers.cpp
+++ src/ttowers.cpp
@@ -31,13 +31,13 @@ KnightTree*  siege(eventList* pEvent,ringsignList* pSarumanList)
 		{
 			code = pEvent->nEventCode / 1000;
 		}
-
+		int knightKey = (pEvent->nEventCode % 10000) / 10;
+		int knightLevel = (pEvent->nEventCode % 10000) % 10;
 	switch (code) 
 	{
 		case 1:
 		{
-			int knightKey = (pEvent->nEventCode % 10000) / 10;
-			int knightLevel = (pEvent->nEventCode % 10000) % 10;
+
 			//add Knight into BST
 			// while duplicate, increase knight's key
 				// new kight's key must differ 777 888 999
@@ -53,12 +53,36 @@ KnightTree*  siege(eventList* pEvent,ringsignList* pSarumanList)
 			// if more than 2 knight, chose the knight has smaller key.
 
 			// increase chosen knight's level (max lvl = 9)
-		}
 
+			// if knight's key = 777
+				// turn tree into list NRL
+				// buil tree with root = 777  
+		}
 			break;
 		case 3: 
 			break;
 		case -1:
+			{
+				//if (tree != NULL)
+					// find the kinght who has key nearest eventKey
+					// if (knight's key = 777 && knight's lvl = monster's lvl)
+						//pass
+					// else if (monster's key = 777 && countLeaf = lvl's monster)
+						// remove all kight that has the depth >= monster's lvl
+					// else if momnster's key = 888
+						// while monster's lvl > knight's lvl && tree != NULL
+								// fight
+					// else
+						// if the knight's lvl >= monster's lvl 
+							//knight wins
+							//
+						// else
+							// knight loses
+							// remove (kight)
+								// (use in-order successor)
+				
+				
+			}
 			break;
 		case 5: 
 			break;
